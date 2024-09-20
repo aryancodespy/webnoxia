685c6341c1cae3a463ba288e049a538ab2ea3fa0
Aryan <125204827+aryancodespy@users.noreply.github.com>
2024-09-20 17:21:57 +0600

    Add and link footer_year js

---
+++ assets/js/footer_year.js
@@ -0,0 +1,13 @@
+
+window.addEventListener('load', onLoad);
+
+function onLoad () {
+    const copyrightEl = document.getElementById('copyright');
+
+    if (copyrightEl) {
+    const currentYear = new Date().getFullYear();
+    const copyrightText = `Copyright &copy; ${currentYear} <a href="#" class="copyright-link">Webnoxia</a>. All Right Reserved`;
+
+    copyrightEl.innerHTML = copyrightText;
+    }
+}
